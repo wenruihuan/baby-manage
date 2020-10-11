5e95a7e8cecc93a891e6a6a4b518fda2c06cb6dc
wenruihuan <30458999+wenruihuan@users.noreply.github.com>
2020-10-11 21:48:06 +0800

    wenruihuan
    xiugai

--- src/components/page/store/employees/roleManagement/addRole.vue
+++ src/components/page/store/employees/roleManagement/addRole.vue
@@ -1,5 +1,42 @@
 <template>
-
+    <div class="add-role">
+        <div class="common-title-one">角色信息</div>
+        <div class="info">
+            <div class="info-box">
+                <div class="title">店铺信息</div>
+                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                    <el-form-item label="归属门店" prop="name">
+                        <el-input :disabled="disabled" v-model="ruleForm.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="角色名称" prop="name">
+                        <el-input :disabled="disabled" v-model="ruleForm.tel"></el-input>
+                    </el-form-item>
+                    <el-form-item label="角色描述" prop="name">
+                        <el-input :disabled="disabled" v-model="ruleForm.tel"></el-input>
+                    </el-form-item>
+                    <!--<el-form-item label=" ">
+                        <el-time-picker
+                                is-range
+                                v-model="ruleForm.time"
+                                range-separator="至"
+                                start-placeholder="开始时间"
+                                end-placeholder="结束时间"
+                                placeholder="选择时间范围">
+                        </el-time-picker>
+                    </el-form-item>-->
+                    <el-form-item label="店铺地址" prop="name">
+                        <!--<el-select></el-select>-->
+                    </el-form-item>
+                    <el-form-item label="店铺介绍">
+                        <el-input type="textarea" :disabled="disabled" v-model="ruleForm.intr"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="shopSave">保存</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
