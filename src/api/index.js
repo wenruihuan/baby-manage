79f61dd2451283ae1bdf9190560cd8c1dcb14d37
wenruihuan <30458999+wenruihuan@users.noreply.github.com>
2020-10-12 20:31:57 +0800

    wenruihuan
    及时沟通页面

--- src/api/index.js
+++ src/api/index.js
@@ -88,3 +88,11 @@ export const roleList = query => {
         params: query
     });
 };
+// 获取角色列表
+export const roleAathList = query => {
+    return request({
+        url: IP + `/admin/role/authList`,
+        method: 'get',
+        params: query
+    });
+};
