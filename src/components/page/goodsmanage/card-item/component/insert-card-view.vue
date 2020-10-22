0bf4eb8474eda0bf277c56bbd7bd25810598e9ad
wb.lvzhiyuan <wb.lvzhiyuan@mesg.corp.netease.com>
2020-10-22 11:59:20 +0800

    开发包厢页面

---
+++ src/components/page/goodsmanage/card-item/component/insert-card-view.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>25</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- src/components/page/goodsmanage/card-item/mock.js
+++ src/components/page/goodsmanage/card-item/mock.js
@@ -128,3 +128,47 @@ export const quanlityList = [
         expire_time: '2020-12-31'
     }
 ];
+
+export const treeData = [{
+    id: 1,
+    label: '一级 1',
+    children: [{
+        id: 4,
+        label: '二级 1-1',
+        children: [{
+            id: 9,
+            label: '三级 1-1-1'
+        }, {
+            id: 10,
+            label: '三级 1-1-2'
+        }]
+    }]
+}, {
+    id: 2,
+    label: '一级 2',
+    children: [{
+        id: 5,
+        label: '二级 2-1'
+    }, {
+        id: 6,
+        label: '二级 2-2'
+    }]
+}, {
+    id: 3,
+    label: '一级 3',
+    children: [{
+        id: 7,
+        label: '二级 3-1'
+    }, {
+        id: 8,
+        label: '二级 3-2'
+    }]
+}];
+
+export const ruleData = [
+    {
+        id: 1,
+        hasSelected: '1',
+        rule: '发送发射点发射点'
+    }
+];
